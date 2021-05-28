b80756314ef99ee6d954bbc123981c3704f6c7fd
gw011211 <gw011211@likelion.org>
2021-05-28 18:01:10 +0900

    로그아웃 추가

--- studyPlanner/diary/views.py
+++ studyPlanner/diary/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import auth
 
 # Create your views here.
 def diary(request):
     #서버의 write 클래스 정보를 모두 가져온다.
     return render(request, 'diary_main.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('diary:diary')
