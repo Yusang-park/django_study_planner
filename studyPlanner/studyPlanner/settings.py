f56dd281a12ab1b0d63e93e574e23517a18b871f
milkness <dfhilder@gmail.com>
2021-05-28 14:08:59 +0900

    123

--- studyPlanner/studyPlanner/settings.py
+++ studyPlanner/studyPlanner/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+#ㅘㅗㅓㅗㅓㅏㅘ치 ㅏ 처ㅏㅣ
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
