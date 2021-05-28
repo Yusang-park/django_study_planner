d642ab7230c949cd92c44157286ca540bd4bd1ae
gw011211 <gw011211@likelion.org>
2021-05-28 16:04:53 +0900

    카카오 로그인

--- studyPlanner/studyPlanner/settings.py
+++ studyPlanner/studyPlanner/settings.py
@@ -39,7 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'diary',
     'calender',
-    'accounts'
+    'accounts',
+
+    #소셜 로그인
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.kakao',
+
 ]
 
 MIDDLEWARE = [
@@ -126,3 +134,18 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+    
+]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+
