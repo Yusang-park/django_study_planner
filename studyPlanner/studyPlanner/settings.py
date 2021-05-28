2dd7f80994b12ecd1151dbb0ab4d177f72f091e3
gw011211 <gw011211@likelion.org>
2021-05-28 17:19:48 +0900

    로그인

--- studyPlanner/studyPlanner/settings.py
+++ studyPlanner/studyPlanner/settings.py
@@ -132,6 +132,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
