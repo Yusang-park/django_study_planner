eabb7a18f7084ec838f7559e3c0857cc152c32f1
gayeon00 <chlrkdus4819@gmail.com>
2021-05-30 11:59:16 +0900

    디자인만 남음

--- studyPlanner/diary/views.py
+++ studyPlanner/diary/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
-from .forms import DailyForm, TodothingForm
+from .models import Profile
+from django.shortcuts import redirect, render
+from .forms import DailyForm, TodothingForm, ProfileForm
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def diary(request):
+    user = request.user
+    message = ''
+    d_day = Profile.objects.get(user = request.user)
+    if request.method == 'POST':
+        daily_form = DailyForm(request.POST)
+        todothing_form = TodothingForm(request.POST)
+        if daily_form.is_valid():
+            todothing_form.save()
+            daily_form.save()
+            return redirect('diary:diary')
+        else:
+            message = 'fail'
+            print(message)
     daily_form = DailyForm()
     todothing_form = TodothingForm()
-    return render(request, 'diary_main.html',{"daily_form":daily_form,"todothing_form":todothing_form} )
+    return render(request, 'diary_main.html',{"daily_form":daily_form,"todothing_form":todothing_form,"message":message,"user":user,"d_day":d_day} )
+
+def setDday(request):
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            return redirect('diary:setDday')
+    profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, "set_dday.html",{"profile_form":profile_form})
+
