4f53dcd8334b8422ce9d260c0ec4bc453d6a8b0c
gayeon00 <chlrkdus4819@gmail.com>
2021-06-21 19:26:23 +0900

    user랑 연동완료

--- studyPlanner/diary/views.py
+++ studyPlanner/diary/views.py
@@ -9,22 +9,29 @@ from .forms import DailyForm, TodothingForm, ProfileForm
 from django.contrib.auth.models import User
 from django.shortcuts import render,redirect
 from django.contrib import auth
+import datetime
+from datetime import date
 
 # Create your views here.
 def diary(request):
+    today = date.today()
+    print('today',today)
     # todothing
-    todo_lists = Todothing.objects.all()
-    todo_form = TodothingForm()
+    # daily = Daily.objects.filter(user = request.user)
     #서버의 write 클래스 정보를 모두 가져온다.
     user = request.user
     if user.is_authenticated:
         d_day = Profile.objects.get(user = request.user)
-             
+        todo_lists = Todothing.objects.filter(user=request.user,date = today)
     else:
-        d_day = 0       
+        d_day = None
+        todo_lists = None
+    todo_form = TodothingForm()       
     if request.method == 'POST':
         daily_form = DailyForm(request.POST)
         if daily_form.is_valid():
+            daily_form = daily_form.save(commit=False)
+            daily_form.user = request.user
             daily_form.save()
             return redirect('diary:diary')
     daily_form = DailyForm()
@@ -32,9 +39,13 @@ def diary(request):
         'todo_form' : todo_form,} )
 
 def addTodo(request):
+    today = date.today()
+    print('today',today)
     if request.method == 'POST':
         todo_form = TodothingForm(request.POST)
         if todo_form.is_valid():
+            todo_form = todo_form.save(commit=False)
+            todo_form.user = request.user
             todo_form.save()
             return redirect('diary:diary')
     else:
