9db151c9d3f6637ff70b05f402847bca7f7eeaad
gw011211 <gw011211@likelion.org>
2021-06-21 10:47:41 +0900

    image

--- studyPlanner/community/models.py
+++ studyPlanner/community/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Write(models.Model):
     title= models.CharField(max_length=50)
     user= models.ForeignKey(User,related_name='write',on_delete=CASCADE,null=True)
-    mainphoto = models.ImageField(null=True,blank=True,upload_to="user_images")
+    mainphoto = models.ImageField(null=True,blank=True)
     contents= models.TextField()
     updated_at= models.DateTimeField(auto_now=True)
     
--- studyPlanner/studyPlanner/urls.py
+++ studyPlanner/studyPlanner/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,7 +26,7 @@ urlpatterns = [
     
     path('community/',include('community.urls')),
     path('accounts/', include('allauth.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
 # 0. 앱 생성, install 설정
