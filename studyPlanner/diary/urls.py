af737f0321cdbbccc4b8b6c7bc6c1122bf6dd9a7
lunar0926 <lunar0926@gmail.com>
2021-06-23 18:39:14 +0900

    jaehyeong2

--- studyPlanner/diary/urls.py
+++ studyPlanner/diary/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('setDday/',views.setDday, name='setDday'),
     path('logout/', views.logout, name='logout'),
     path('setDiary/', views.setDiary, name='setDiary'),
+    path('diary/<int:year>/<int:month>/<int:day>/', views.changed_diary, name="changed_diary"),
 ]
